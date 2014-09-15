f1af0c68c50175b60afd641eb980098101559283
ch22iu <ch22iu@student.lnu.se>
2014-09-15 16:10:48 +0200

    deleted js

--- html/js/loadingMobil.js
+++ html/js/loadingMobil.js
@@ -1,14 +1,13 @@
 
 var isMobile = {
     Android: function() {
-	
+		loadMobilCss();
         return navigator.userAgent.match(/Android/i);
     },
     BlackBerry: function() {
         return navigator.userAgent.match(/BlackBerry/i);
     },
     iOS: function() {
-		
         return navigator.userAgent.match(/iPhone|iPad|iPod/i);
     },
     Opera: function() {
@@ -18,22 +17,26 @@ var isMobile = {
         return navigator.userAgent.match(/IEMobile/i);
     },
 	WindowsOS: function() {
+	
 		return navigator.userAgent.match(/Windows/i);
 	},
     any: function() {
         return (isMobile.Android() || isMobile.BlackBerry() || is.WindowsOS() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
     }
 };
+
+
+
 var fileref = document.createElement("link");
-var loadMobilCss = function() {
+var loadMobilCss = function() 
+{
 		cssPath = "html/css/style_mobile.css";
 		fileref.setAttribute("rel", "stylesheet");
 		fileref.setAttribute("type", "text/css");
 		fileref.setAttribute("href", cssPath);
 		document.getElementsByTagName("head")[0].appendChild(fileref);
+		alert('tja');
 }
-
-if( isMobile.any() ) alert('You are using a mobilephone!!!');
 
 
 // Testing
